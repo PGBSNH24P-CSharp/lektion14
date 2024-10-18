811c56402bbebda43b00def1c6e47791e89fd5c5
EnanderW <williamenander@gmail.com>
2024-10-18 12:54:29 +0200

    add generics example code

---
+++ Optional.cs
@@ -0,0 +1,27 @@
+public class Optional<T> {
+    T value;
+
+    public static Optional<T> Of(T value) {
+        return new Optional<T> {
+            value = value,
+        };
+    }
+
+    public static Optional<T> Empty() {
+        return new Optional<T> {
+            value = null,
+        };
+    }
+
+    public bool IsEmpty() {
+        return value == null;
+    }
+
+    public bool IsPresent() {
+        return value != null;
+    }
+
+    public T Get() {
+        return value;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,86 @@
 ﻿namespace lektion14;
 
+public interface ISomeInterface {
+    void DoSomething();
+}
+
+public class SomeClass : ISomeInterface
+{
+    public void DoSomething() {}
+}
+
 class Program
 {
-    static void Main(string[] args) { }
+    public static Optional<SomeClass> GetSomeValue() {
+        Random random = new Random();
+        if (random.NextDouble() < 0.5) {
+            return Optional<SomeClass>.Empty();
+        } else {
+            return Optional<SomeClass>.Of(new SomeClass());
+        }
+    }
+
+    static void Main(string[] args) { 
+        // Klasser
+        // Metoder
+        // Constraints
+
+        // Optional
+        // Sök i array
+
+        // Fel, eftersom 'int' inte ärver ifrån 'ISomeInterface'
+        MyList<int> numbers;
+        // Fel, eftersom 'string' inte ärver ifrån 'ISomeInterface'
+        MyList<string> text;
+        // Ok
+        MyList<SomeClass> someList;
+
+        PrintValue<string>("Hej!");
+        PrintValue<int>(5);
+        PrintValue<Random>(new Random());
+
+        int? value;
+
+        MyDictionary<string, int> dict;
+        MyDictionary<double, Random> dict2;
+
+        Optional<SomeClass> optional = GetSomeValue();
+        if (optional.IsEmpty()) {
+            Console.WriteLine("Value is null");
+            return;
+        }
+
+        SomeClass actualValue = optional.Get();
+
+        //
+        SomeClass? someValue = SomeFunction();
+
+        Contains([1, 2, 3, 4], 5);
+    }
+
+    public static bool Contains<T>(T[] array, T value) {
+        foreach (T all in array) {
+            if (all.Equals(value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void PrintValue<T>(T value) {
+        Console.WriteLine(value);
+    }
+}
+
+public class MyDictionary<K, V> {}
+
+public class MyList<T> where T : ISomeInterface {
+    T[] nums;
+
+    public void Add() {
+        nums[0].DoSomething();
+    }
+
+    public void Remove() {}
 }
